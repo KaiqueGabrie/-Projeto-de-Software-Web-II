84cf39d83d53087f1f94039b06803303c9e0ec7a
Rafaela Pereira da Silva <pereira.r@estudante.ifro.edu.br>
2025-09-03 17:29:32 -0400

    first commit

---
+++ Desktop/Atividade 2/exercicio5.js
@@ -0,0 +1,12 @@
+// problema5.js
+
+let qtdP = 3; // pequenas
+let qtdM = 2; // médias
+let qtdG = 1; // grandes
+
+let total = qtdP * 10 + qtdM * 12 + qtdG * 15;
+
+console.log("Quantidade P:", qtdP);
+console.log("Quantidade M:", qtdM);
+console.log("Quantidade G:", qtdG);
+console.log("Valor arrecadado: R$", total);
